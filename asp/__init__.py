54bdb64cc624bc07f1a70bd82bf3533bb8f3ba64
Tom Wallis <github@tomwallis.net>
2018-04-09 16:20:28 +0100

    Added tests for prelude, encore decorators

--- asp/__init__.py
+++ asp/__init__.py
@@ -1,2 +1,2 @@
 from .weaver import weave_clazz, weave_module, unweave_all_classes, unweave_class
-from .weaver import IdentityAspect
+from .weaver import IdentityAspect, identity, prelude, encore
--- tests/test_weaver.py
+++ tests/test_weaver.py
@@ -1,5 +1,5 @@
 import unittest
-from asp import weave_clazz
+from asp import weave_clazz, prelude, encore
 
 
 class CountingAspect(object):
@@ -15,7 +15,14 @@ class CountingAspect(object):
 
 class Target(object):
 
+    def __init__(self):
+        self.count = 0
+
     def foo(self):
+        return self
+
+    def increment_count(self):
+        self.count += 1
         return self
 
 
@@ -30,6 +37,34 @@ class WeaverTestCase(unittest.TestCase):
         Target().foo().foo().foo()
 
         self.assertEqual(3, counting_aspect.invocations)
+
+    def test_prelude_decorator(self):
+
+        @prelude
+        def set_count_to_5_before_running(attr, context, *args, **kwargs):
+            context.count = 5
+
+        advice = {Target.increment_count: set_count_to_5_before_running}
+        weave_clazz(Target, advice)
+
+        target = Target()
+        target.increment_count()
+
+        self.assertEqual(target.count, 5+1)
+
+    def test_encore_decorator(self):
+
+        @encore
+        def set_count_to_5_after_running(attr, context, *args, **kwargs):
+            context.count = 5
+
+        advice = {Target.increment_count: set_count_to_5_after_running}
+        weave_clazz(Target, advice)
+
+        target = Target()
+        target.increment_count()
+
+        self.assertEqual(target.count, 5)
 
 
 if __name__ == '__main__':
