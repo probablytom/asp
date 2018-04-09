54b728354dcb9c6d26ad9c8052dcfaf881ed92cf
Tom Wallis <github@tomwallis.net>
2018-04-10 00:56:40 +0100

    Finished advicebuilder, tests pass

--- asp/advice_builder.py
+++ asp/advice_builder.py
@@ -1,4 +1,10 @@
+'''
+A nice API to build advice from.
+@author probablytom
+'''
 from .weaver import IdentityAspect, weave_clazz
+from functools import partial
+
 
 class FlexibleAdvice(IdentityAspect):
     def __init__(self, target):
@@ -6,6 +12,7 @@ class FlexibleAdvice(IdentityAspect):
         self.preludes = []
         self.encores = []
         self.error_handlers = []
+        self.around_functions = []
         self.target = target
 
     def prelude(self, attribute, context, *args, **kwargs):
@@ -20,6 +27,17 @@ class FlexibleAdvice(IdentityAspect):
             raise exception
 
         [handler(attribute, context, exception) for handler in self.error_handlers]
+
+    def around(self, attribute, context, *args, **kwargs):
+
+        def nest_around_call(nested_around, next_around):
+            return partial(next_around, nested_around, context)
+
+        nested_around = reduce(nest_around_call,
+                               self.around_functions[::-1],
+                               partial(super(FlexibleAdvice, self).around, attribute, context))
+
+        return nested_around(*args, **kwargs)
 
     def apply_advice(self):
         weave_clazz(self.target.im_class, {self.target: self})
@@ -44,6 +62,12 @@ class AdviceBuilder(object):
     def add_error_handler(self, target, err_handler):
         specific_advice = self.advice.get(target, FlexibleAdvice(target))
         specific_advice.error_handlers.append(err_handler)
+        self.advice[target] = specific_advice
+        return self
+
+    def add_around(self, target, around):
+        specific_advice = self.advice.get(target, FlexibleAdvice(target))
+        specific_advice.around_functions.append(around)
         self.advice[target] = specific_advice
         return self
 
--- tests/test_advice_builder.py
+++ tests/test_advice_builder.py
@@ -1,5 +1,5 @@
 import unittest
-from asp import AdviceBuilder
+from asp import AdviceBuilder, generate_around_advice
 
 class Target(object):
     def __init__(self):
@@ -78,3 +78,59 @@ class TestAdviceBuilder(unittest.TestCase):
         target = Target()
         target.increment_count()
         self.assertEqual(target.count, 1+1+1)  # Should have incremented, then incremented twice more by two encores.
+
+    def test_advice_builder_around(self):
+        def around_to_test(attribute, context, *args, **kwargs):
+            '''
+            Add three, run the original (going to add another one), then multiply by two.
+            '''
+            context.count += 3
+            result = attribute(*args, **kwargs)
+            context.count *= 2
+            return result
+
+        builder = AdviceBuilder()
+        builder.add_around(Target.increment_count, around_to_test)
+        builder.apply()
+
+        target = Target()
+        target.increment_count()
+        self.assertEqual(target.count, (3+1) * 2)
+
+    def test_generated_advice(self):
+        def add_three(attribute, context, *args, **kwargs):
+            context.count += 3
+
+        def mult_by_2(attribute, context, result):
+            context.count *= 2
+
+        generated_around = generate_around_advice(add_three, mult_by_2)
+        builder = AdviceBuilder()
+        builder.add_around(Target.increment_count, generated_around)
+        builder.apply()
+
+        target = Target()
+        target.increment_count()
+        self.assertEqual(target.count, (3+1) * 2)
+
+    def test_multiple_arounds(self):
+        def around_to_test(attribute, context, *args, **kwargs):
+            '''
+            Add three, run the original (going to add another one), then multiply by two.
+            '''
+            context.count += 3
+            result = attribute(*args, **kwargs)
+            context.count *= 2
+            return result
+
+        builder = AdviceBuilder()
+        builder.add_around(Target.increment_count, around_to_test)
+        builder.add_around(Target.increment_count, around_to_test)
+        builder.apply()
+
+        target = Target()
+        target.increment_count()
+        # We want arounds to wrap around each other, so we want to add three twice, then increment, then mult by 4.
+        self.assertEqual(target.count, ((3+(3+1))*2)*2)
+
+
